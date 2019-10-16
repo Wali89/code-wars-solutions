410d6e05a69e6cc1659cbd3f950a51a5eb731994
Wali Rahman <wali.rahman89@yahoo.com>
2019-10-15 21:17:32 -0400

    Create odd-or-even.js

---
+++ odd-or-even.js
@@ -0,0 +1,14 @@
+function oddOrEven(ar) {
+  var sum = 0;
+  
+  for(var i=0; i < ar.length; i++) {
+    ans+= ar[i]
+  }
+  
+  if(sum == 0 || ans % 2 == 0) {
+    return 'even'
+  } else {
+   return'odd'
+  }
+
+}
