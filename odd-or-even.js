fe1612dd9ca0f7fd51fa349d8769b7bbe0fcab12
Wali Rahman <wali.rahman89@yahoo.com>
2019-10-15 21:21:17 -0400

    Better solution

--- odd-or-even.js
+++ odd-or-even.js
@@ -1,14 +1,4 @@
 function oddOrEven(ar) {
-  var sum = 0;
-  
-  for(var i=0; i < ar.length; i++) {
-    ans+= ar[i]
-  }
-  
-  if(sum == 0 || ans % 2 == 0) {
-    return 'even'
-  } else {
-   return'odd'
-  }
+  return ar.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
 
 }
