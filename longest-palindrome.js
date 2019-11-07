2d1e5951fb08b9ed6a27664d1ab5c00f1238a1f8
Wali Rahman <wali.rahman89@yahoo.com>
2019-11-07 09:09:20 -0500

    Create longest-palindrome.js

---
+++ longest-palindrome.js
@@ -0,0 +1,9 @@
+var longestPalindrome=function(s){
+  if (!s) return 0;
+  for (let c = s.length; c > 0; c--) {
+    for (let i = 0; i <= s.length - c; i++) {
+      var check = s.substr(i, c);
+      if (check === check.split("").reverse().join("")) return c;
+    }
+  }
+}
