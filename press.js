2ad8853129bdc85f870707ba1efd8733dbca6094
Wali Rahman <wali.rahman89@yahoo.com>
2019-11-23 18:54:45 -0500

    Create press.js

---
+++ press.js
@@ -0,0 +1,15 @@
+function presses(phrase) {
+  var chunks = ['1', 'ABC2', 'DEF3', 'GHI4', 'JKL5', 'MNO6', 'PQRS7', 'TUV8', 'WXYZ9', ' 0'],
+      phrase = phrase.toUpperCase().split(''),
+      total = 0;
+  
+  phrase.forEach(function(l) {
+    var key = chunks.filter(function(c) {
+      return c.indexOf(l) > -1;
+    })[0];
+    total += key.indexOf(l) + 1;
+  });
+  
+  return total;
+      
+}
