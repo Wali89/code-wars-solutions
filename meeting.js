53ffc5bbcc0c7d2f0dbfa2e6f51b5d17f3583e2e
Wali Rahman <wali.rahman89@yahoo.com>
2019-11-24 20:43:42 -0500

    Create meeting.js

---
+++ meeting.js
@@ -0,0 +1,7 @@
+function meeting(s) {
+  let string = s.toUpperCase().split(';')
+                .map(x => x.split(':').reverse().join(', '))
+                .sort()
+                .join(')(')
+  return '(' + string + ')'
+}
